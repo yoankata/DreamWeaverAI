2440f6d5feb3d81c274f558b1f0aae8f2da14816
yoankata <32326965+yoankata@users.noreply.github.com>
2024-08-28 18:05:09 +0100

    Update home.py

--- screens/home.py
+++ screens/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="DreamWeaver AI", page_icon=":flag-ee", layout="centered", initial_sidebar_state="auto")
 
 def app():
     st.title("Welcome to DreamWeaver AI")
