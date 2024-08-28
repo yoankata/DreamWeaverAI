65313a37302671703d010ef97bff947aea35c80b
yoankata <32326965+yoankata@users.noreply.github.com>
2024-08-28 17:58:58 +0100

    Update home.py

--- screens/home.py
+++ screens/home.py
@@ -6,4 +6,4 @@ def app():
     st.title("Welcome to DreamWeaver AI")
     video_file = open("videos/DreamWeaverAILogovideo.mp4", "rb")
     video_bytes = video_file.read()
-    st.video(video_bytes, format="video/mp4", loop=True, autoplay=True)
+    st.video(video_bytes, format="video/mp4", loop=True, autoplay=True, start_time=0, use_container_width=True)
