6d79762d0f2bea8606a8e08cad76a0dc4423dd99
yoankata <32326965+yoankata@users.noreply.github.com>
2024-08-28 18:20:43 +0100

    Update home.py

--- screens/home.py
+++ screens/home.py
@@ -3,17 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="DreamWeaver AI", page_icon=":flag-ee:", layout="centered", initial_sidebar_state="auto")
 
 def app():
-    # CSS to set the page height to 70vh
-    st.markdown("""
-        <style>
-        html, body {
-            height: 70vh;
-            margin: 0;
-            padding: 0;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-    st.title("Welcome to DreamWeaver AI")
+    st.markdown("<center>Welcome to DreamWeaver AI</center>", unsafe_allow_html=True)
     with open("videos/DreamWeaverAILogovideo.mp4", "rb") as video_file:
         video_bytes = video_file.read()
     
