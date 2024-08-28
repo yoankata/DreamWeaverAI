70034479471502ba9db58e2d0c9a02d7af89e26f
yoankata <32326965+yoankata@users.noreply.github.com>
2024-08-28 17:03:49 +0100

    Update home.py
    Removing redundant parameter start video at 0 (might be messing up autoplay).

--- screens/home.py
+++ screens/home.py
@@ -4,4 +4,4 @@ def app():
     st.title("Welcome to DreamWeaver AI")
     video_file = open("videos/DreamWeaverAILogovideo.mp4", "rb")
     video_bytes = video_file.read()
-    st.video(video_bytes, format="video/mp4", start_time=0, loop=True, autoplay=True)
+    st.video(video_bytes, format="video/mp4", loop=True, autoplay=True)
