d68d85e9d1a68699b0f5d9bed2965db49906c5ef
yoankata <32326965+yoankata@users.noreply.github.com>
2024-08-28 17:42:43 +0100

    Update home.py

--- screens/home.py
+++ screens/home.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 def app():
     st.set_page_config(page_title="DreamWeaver AI" ,page_icon=None, layout="centered", initial_sidebar_state="auto")
-    DreamWeaver AI
     st.title("Welcome to DreamWeaver AI")
     video_file = open("videos/DreamWeaverAILogovideo.mp4", "rb")
     video_bytes = video_file.read()
