178f2b471c214736be54a547e3bf2115684e9b17
yoankata <32326965+yoankata@users.noreply.github.com>
2024-08-28 18:10:31 +0100

    Update home.py

--- screens/home.py
+++ screens/home.py
@@ -6,4 +6,14 @@ def app():
     st.title("Welcome to DreamWeaver AI")
     with open("videos/DreamWeaverAILogovideo.mp4", "rb") as video_file:
         video_bytes = video_file.read()
+    st.markdown("""
+        <style>
+        .stVideo {
+            width: 100%;
+            max-width: 800px;  # Adjust this value as needed
+            margin: 0 auto;
+            display: block;
+        }
+        </style>
+        """, unsafe_allow_html=True)
     st.video(video_bytes, format="video/mp4", loop=True, autoplay=True)
