fc01b66752aa86431c7b2f11cef7db85b6793eea
Ben Hall <bxh-io@users.noreply.github.com>
2024-09-15 17:45:19 -0400

    chore: liniting

--- screens/fridge_me.py
+++ screens/fridge_me.py
@@ -7,7 +7,8 @@ def app():
 
     st.title("What's for (healthy) dinner?")
 
-    st.text("Take a picture of your fridge to see what you can make for dinner?")
+    st.text(
+        "Take a picture of your fridge to see what you can make for dinner?")
     col1, col2 = st.columns([3, 3])
 
     response = ""
@@ -15,7 +16,6 @@ def app():
         uploaded_file = st.file_uploader("Upload a picture of your fridge")
         if uploaded_file is not None:
             response = fridge_results(uploaded_file.getvalue())
-            # col1.markdown(f"<span style='word-wrap:break-word;'>{response}", unsafe_allow_html=True)
 
     with col2:
         st.image("./images/fullsizerender-287.jpg")
@@ -23,4 +23,5 @@ def app():
         if st.button("Generate ideas", key="123"):
             with st.spinner(f'Musing....'):
                 response = fridge_results()
-    st.markdown(f"<span style='word-wrap:break-word;'>{response}", unsafe_allow_html=True)
+    st.markdown(f"<span style='word-wrap:break-word;'>{response}",
+                unsafe_allow_html=True)
